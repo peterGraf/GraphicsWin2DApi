c89f1cf717c58122568ce118c4fd6563408a777a
Peter <Peter.Robert.Graf@gmail.com>
2018-01-25 00:43:57 +0100

    Update AssemblyInfo.cs

--- GraphicsWin2D/p01GameOfLifeLines/Properties/AssemblyInfo.cs
+++ GraphicsWin2D/p01GameOfLifeLines/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("p01GameOfLifeLines")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Peter Graf 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
